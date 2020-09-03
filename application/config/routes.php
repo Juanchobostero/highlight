a379cd72dfa113e2ff6f7a5a74c03120a781c6d3
tupak <soportesoftcre@gmail.com>
2020-09-03 12:08:18 -0300

    03.09.2020

---
+++ application/config/routes.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$route = array(
+  'default_controller'    => 'Inicio_controller',
+  '404_override'          => '',
+  'translate_uri_dashes'  => FALSE,
+
+);
